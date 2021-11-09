21d9a2a8023b436a1a2cd096769f1c2b9105144e
Calin606 <calin.galan@student.usv.ro>
2021-11-09 20:16:34 +0200

    Okto config server init

---
+++ okto-config-server/src/main/java/com/university/oktoconfigserver/OktoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.university.oktoconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktoConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OktoConfigServerApplication.class, args);
+    }
+
+}
