3ddc4f7f06f2f4c5b6d8136644a3d997a27b69cd
Calin606 <calin.galan@student.usv.ro>
2021-11-14 21:44:11 +0200

    Order service init

---
+++ okto-order-service/src/main/java/com/university/oktoorderservice/OktoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.university.oktoorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktoOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OktoOrderServiceApplication.class, args);
+    }
+
+}
