e3e70bbb9ebe218c43b2ea039151c8cc5c11026b
Calin606 <calin.galan@student.usv.ro>
2021-11-15 23:37:34 +0200

    Configuration server init

---
+++ okto-configuration-server/src/test/java/com/university/oktoconfigurationserver/OktoConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.oktoconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktoConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
