faab790ed9e21bc9ace15f16dc6144dbd08866bd
Calin606 <calin.galan@student.usv.ro>
2021-11-30 20:40:53 +0200

    Add inventory service

---
+++ okto-inventory-service/src/main/java/com/university/oktoinventoryservice/OktoInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.university.oktoinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktoInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OktoInventoryServiceApplication.class, args);
+    }
+
+}
