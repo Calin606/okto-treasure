0b2f02ad39542b04aa3781838d42f69c99b1a557
Calin606 <calin.galan@student.usv.ro>
2021-12-11 23:36:54 +0200

    Add order configuration for inventory

--- okto-order-service/src/main/java/com/university/oktoorderservice/OktoOrderServiceApplication.java
+++ okto-order-service/src/main/java/com/university/oktoorderservice/OktoOrderServiceApplication.java
@@ -3,42 +3,36 @@ package com.university.oktoorderservice;
 //import feign.RequestInterceptor;
 //import lombok.RequiredArgsConstructor;
 //import org.springframework.beans.factory.BeanFactory;
+import feign.RequestInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.feign.EnableFeignClients;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableFeignClients
-//@RequiredArgsConstructor
+@EnableFeignClients
 public class OktoOrderServiceApplication {
-
-//    private final BeanFactory beanFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(OktoOrderServiceApplication.class, args);
     }
 
-//    @Bean
-//    public RequestInterceptor requestTokenBearerInterceptor() {
-//        return requestTemplate -> {
-//            JwtAuthenticationToken token = (JwtAuthenticationToken) SecurityContextHolder.getContext()
-//                    .getAuthentication();
-//
-//            requestTemplate.header("Authorization", "Bearer " + token.getToken().getTokenValue());
-//        };
-//    }
-//
-//    @Bean
-//    public ExecutorService traceableExecutorService() {
-//        ExecutorService executorService = Executors.newCachedThreadPool();
-//        return new TraceableExecutorService(beanFactory, executorService);
-//    }
+    @Bean
+    public RequestInterceptor requestTokenBearerInterceptor() {
+        return requestTemplate -> {
+            JwtAuthenticationToken token = (JwtAuthenticationToken) SecurityContextHolder.getContext()
+                    .getAuthentication();
+
+            requestTemplate.header("Authorization", "Bearer " + token.getToken().getTokenValue());
+        };
+    }
 
 }
