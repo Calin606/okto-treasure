6482858458a1bd15ed692fa1eacb79e093ca2153
GalanSebastian <sebastian.galan@assist.ro>
2022-03-26 12:40:33 +0200

    Notification service and Event Driven Microservices

---
+++ okto-notification-service/src/test/java/com/university/oktonotificationservice/OktoNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.oktonotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktoNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
