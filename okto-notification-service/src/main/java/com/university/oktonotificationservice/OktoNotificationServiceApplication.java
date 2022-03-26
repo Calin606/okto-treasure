006efe9dc3dc8792a2d5f87d550551f4a00764c8
GalanSebastian <sebastian.galan@assist.ro>
2022-03-26 10:36:52 +0200

    Add okto-notification-service

---
+++ okto-notification-service/src/main/java/com/university/oktonotificationservice/OktoNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.university.oktonotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktoNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktoNotificationServiceApplication.class, args);
+	}
+
+}
